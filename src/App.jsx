28f60a14b39f0fe063abc890045b7f77bb93215b
richard <richard@provalidator.com>
2024-12-12 16:39:11 +0900

    function del

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,6 @@ import "./App.css";
 import DateCard from "./components/DateCard";
 import data from "./data.json";
 function App() {
-  function blank(data) {
-    if (data[0].date % 7 === 1) {
-      {
-        for (let i = 0; i < 7 - data[0].date; i++) {
-          <li></li>;
-        }
-      }
-    } else {
-      {
-        data?.map((v, i) => (
-          <DateCard
-            key={i}
-            date={v.date}
-            todos={v.todos}
-            isHoliday={v.isHoliday}
-          />
-        ));
-      }
-    }
-  }
   return (
     <div className="app-container">
       <h1>December, 2024</h1>
@@ -35,7 +15,6 @@ function App() {
         <li>SUN</li>
       </ul>
       <ul className="date-list">
-        {blank(data)}
         {data?.map((v, i) => (
           <DateCard
             key={i}
