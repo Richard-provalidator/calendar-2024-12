a1802efaaa05e91596516e2c39e9932889ba8a36
richard <richard@provalidator.com>
2024-12-13 09:00:40 +0900

    weekday function add

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import "./App.css";
 import DateCard from "./components/DateCard";
 import data from "./data.json";
 function App() {
+  const weekdays = Array.from({ length: 7 }, (_, i) => {
+    const date = new Date(2024, 11, i + 1); // 2024년 12월 1일을 기준으로 요일 생성
+    return new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(date); // 약어 생성
+  });
   return (
     <div className="app-container">
       <h1>December, 2024</h1>
       <ul className="day-list">
-        <li>SUN</li>
-        <li>MON</li>
-        <li>TUE</li>
-        <li>WED</li>
-        <li>THU</li>
-        <li>FRI</li>
-        <li>SAT</li>
+        {weekdays.map((v, i) => (
+          <li key={i}>{v}</li>
+        ))}
       </ul>
       <ul className="date-list">
         {[...Array(dayNumber(data))].map((_, i) => (
